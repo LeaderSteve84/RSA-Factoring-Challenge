a06cb5bf19d0bc31d10c8ad2d57234a994c09ad8
stephen_adah <mailstephenreal@gmail.com>
2023-10-01 14:03:32 -0700

    0. Factorize all the things!

--- factors.c
+++ factors.c
@@ -1,44 +1,87 @@
 #include "factors.h"
 /**
  */
-void factorise(int n)
+int64_t gcd(int64_t a, int64_t b)
 {
-	int i;
-
-	for (i = 2; i <= n / 2; i++)
+	if (b == 0)
 	{
-		if (n % i == 0)
-		{
-			printf("%d=%d*%d\n", n, i, n / i);
-		}
+		return a;
 	}
+	return gcd(b, a % b);
 }
 /**
  */
+int64_t pollards_rho(int64_t n)
+{
+	int64_t x = 2, y = 2, d = 1;
 
-int main(int argc, char *argv)
+	while (d == 1)
+	{
+		x = (x * x + 1) % n;
+		y = (y * y + 1) % n;
+		y = (y * y + 1) % n;
+		d = gcd(labs(x - y), n);
+	}
+	return (d);
+}
+/**
+ */
+void factorise(int64_t n)
+{
+	int64_t factor;
+
+	if (n <= 1)
+	{
+		return;
+	}
+
+	if (n % 2 == 0)
+	{
+		printf("%ld=%ld * 2\n", n, n / 2);
+		return;
+	}
+
+	factor = pollards_rho(n);
+	if (factor == n)
+	{
+		printf("%ld=%ld*1\n", n, n);
+	}
+	else
+	{
+		printf("%ld=%ld*%ld\n", n, n / factor, factor);
+	}
+}
+
+/**
+ */
+
+int main(int argc, char *argv[])
 {
 	FILE *file;
 	char buffer[100];
-	int num;
+	long int num;
 
 	if (argc != 2)
+	{
 		printf("Usage: %s <file>\n", argv[0]);
 		return (1);
+	}
 	
 	file = fopen(argv[1], "r");
 	if (file == NULL)
+	{
 		perror("Error opening file");
 		return (1);
+	}
 
 	while (fgets(buffer, sizeof(buffer), file))
 	{
-		num = atoi(buffer);
+		num = atol(buffer);
 		if (num <= 1)
 		{
 			continue;
 		}
-		factorise(n);
+		factorise(num);
 	}
 	fclose(file);
 	return (0);
--- factors.h
+++ factors.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int64_t gcd(int64_t a, int64_t b);
+int64_t pollards_rho(int64_t n);
+void factorise(int64_t n);
 
 
-
-#define
+#endif
